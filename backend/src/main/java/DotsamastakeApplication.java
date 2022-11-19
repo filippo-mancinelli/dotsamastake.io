c6a8e8dea1bab6d1fbb4810e29b63e222090c358
filippo-mancinelli <73702913+filippo-mancinelli@users.noreply.github.com>
2022-11-19 10:37:35 +0100

    Project structure

---
+++ backend/src/main/java/DotsamastakeApplication.java
@@ -0,0 +1,14 @@
+package src.main.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DotsamastakeApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotsamastakeApplication.class, args);
+	}
+}
+
