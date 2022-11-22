2c3f440ce01a362cd21ea6ae7df2ca40d104c5e3
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2022-11-22 13:03:23 +0100

    maven conf 2

--- backend/src/main/java/DotsamastakeApplication.java
+++ backend/src/main/java/DotsamastakeApplication.java
@@ -1,4 +1,4 @@
-package src.main.java;
+package backend.src.main.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/main/java/TomcatController.java
+++ backend/src/main/java/TomcatController.java
@@ -1,4 +1,4 @@
-package src.main.java;
+package backend.src.main.java;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
