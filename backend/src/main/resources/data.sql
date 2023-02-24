425c87fa79e882f0ec34db5be489d9c3e0bac860
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2023-02-24 11:20:42 +0000

    fresh

--- backend/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO utenti VALUES (1, 'Carl', 'carl@gmail.org');    
-INSERT INTO utenti VALUES (2, 'Lepre', 'carl@gmail.net');    
-INSERT INTO utenti VALUES (3, 'pollo', 'yachty@dotmail.com');    
