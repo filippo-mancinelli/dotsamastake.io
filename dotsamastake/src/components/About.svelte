15a201aeee41e38682bd59cc9e2ee0560f59c60f
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2023-02-28 11:41:21 +0000

    About section

---
+++ dotsamastake/src/components/About.svelte
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+
+<section class="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24 tails-selected-element" contenteditable="true">
+    <div class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
+
+        <!-- Image -->
+        <div class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
+            <img src="https://cdn.devdojo.com/tails/images/UCRgeU7jRcCSMEpQ8H6QTRZNgbyaOLTQJ5CVJxZk.jpg" class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20">
+        </div>
+
+        <!-- Content -->
+        <div class="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none lg:font-mono">
+            <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl lg:font-mono">
+                Stake with us</h2>
+            <p class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">By nominating our validator nodes, you are actively supporting the polkadot and/or kusama network, while earning high rewards<span style="background-color: rgb(254, 202, 202);" class=""></span></p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid lg:font-mono lg:text-sm">
+                    <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full lg:bg-red-200" data-primary="yellow-400"><span class="text-sm font-bold">✓</span></span> Maximize productivity and growth
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid lg:text-sm lg:font-normal lg:font-mono">
+                    <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full lg:bg-red-200" data-primary="yellow-400"><span class="text-sm font-bold">✓</span></span> Speed past your competition
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid lg:font-light lg:text-sm tails-preview lg:font-mono">
+                    <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full lg:bg-red-200" data-primary="yellow-400"><span class="text-sm font-bold">✓</span></span> Learn the top techniques
+                </li>
+            </ul>
+        </div>
+        <!-- End  Content -->
+    </div>
+    
+</section>
