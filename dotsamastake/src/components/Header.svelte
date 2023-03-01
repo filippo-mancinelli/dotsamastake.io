bb8276dc67004d045865a0019af92858a341859e
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2023-03-01 11:15:30 +0000

    Fixed session status of Connection, added arrow

--- dotsamastake/src/components/Header.svelte
+++ dotsamastake/src/components/Header.svelte
@@ -1,4 +1,6 @@
 <script>
+// @ts-nocheck
+
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { isWeb3Injected, web3Accounts, web3AccountsSubscribe, web3Enable } from '@polkadot/extension-dapp';
 import { onMount, onDestroy } from 'svelte';
@@ -9,15 +11,31 @@ import Dropdown from 'sv-bootstrap-dropdown';
 export let isConnected = writable(localStorage.getItem('isConnected') === 'true');
 let showButton = writable(localStorage.getItem('isConnected') === 'true');
 let buttonColors = writable('');
-let unsubscribe;
 let dropdownTrigger;
+
+// we subscribe to any account change and log the new list. We also check if there is no account connected so we can update isConnected acccordingly
+const unsubscribe = async () => {
+  await web3AccountsSubscribe(( injectedAccounts ) => { 
+        injectedAccounts.map(( accounts ) => {
+            console.log(accounts.address); 
+        });
+
+        if(injectedAccounts.length === 0) {
+          isConnected.set(false);
+          console.log($isConnected)
+        } else {
+          isConnected.set(true);
+          console.log($isConnected)
+        }
+    });
+}
 
 //reactive block that updates the colors of the button whenever 'isConnected' changes
 $: {
   if($isConnected) {
     isConnected.set(true);
     console.log($isConnected)
-    buttonColors.set('text-white bg-zinc-900');
+    buttonColors.set('text-white bg-black');
     showButton.set(true);
   } else {
     isConnected.set(false);
@@ -31,22 +49,8 @@ async function connectWallet() {
   let extension = await web3Enable('dotsamastake.io');
   
   if (extension.length > 0) { //check if the extension is installed
-
-    // we subscribe to any account change and log the new list. We also check if there is no account connected so we can update isConnected acccordingly
-    unsubscribe = await web3AccountsSubscribe(( injectedAccounts ) => { 
-        injectedAccounts.map(( accounts ) => {
-            console.log(accounts.address); 
-        });
-
-        if(injectedAccounts.length === 0) {
-          isConnected.set(false);
-          console.log($isConnected)
-        } else {
-          isConnected.set(true);
-          console.log($isConnected)
-        }
-    });
-
+    //we subscribe to changes
+    unsubscribe();
   } else {
     console.log('Polkadot JS Wallet Extension is not installed, or user rejected to connect');
   }
@@ -79,25 +83,30 @@ onDestroy(() => {
     </a>
     <div class="w-full md:flex hidden md:flex-row flex-col md:relative absolute h-screen md:pt-0 pt-20 md:h-full items-stretch justify-between">
         <nav class="w-full md:space-x-8 font-medium md:bg-transparent bg-[#FFFDF8] md:px-8 h-full flex md:flex-row flex-col z-20 items-center">
-            <a href="#_" class="hover:underline md:border-b-0 text-center py-6 border-b-[3px] border-gray-900 md:w-auto w-full 2xl:font-mono 2xl:font-light 2xl:text-xl">Home</a>
-            <a href="#_" class="hover:underline md:border-b-0 text-center py-6 border-b-[3px] border-gray-900 md:w-auto w-full 2xl:font-mono 2xl:font-light 2xl:text-xl">About</a>
-            <a href="#_" class="hover:underline md:border-b-0 text-center py-6 border-b-[3px] border-gray-900 md:w-auto w-full 2xl:font-mono 2xl:font-light 2xl:text-xl">Contact</a>
+            <a href="#home" class="hover:underline md:border-b-0 text-center py-6 border-b-[3px] border-gray-900 md:w-auto w-full 2xl:font-mono 2xl:font-light 2xl:text-xl">Home</a>
+            <a href="#about" class="hover:underline md:border-b-0 text-center py-6 border-b-[3px] border-gray-900 md:w-auto w-full 2xl:font-mono 2xl:font-light 2xl:text-xl">About</a>
+            <a href="#contact" class="hover:underline md:border-b-0 text-center py-6 border-b-[3px] border-gray-900 md:w-auto w-full 2xl:font-mono 2xl:font-light 2xl:text-xl">Contact</a>
         </nav>
 
-        <div class="flex flex-shrink-0 md:flex-row flex-col md:bg-transparent bg-[#FFFDF8] font-medium border-l-0 md:border-l-[3px] border-gray-900 items-center">
+        <div class="{$buttonColors} flex flex-shrink-0 md:flex-row flex-col md:bg-transparent font-medium border-l-0 md:border-l-[3px] border-gray-900 items-center" style="background-color: {$isConnected ? 'black' : 'white'}">
           {#if !$isConnected}
-            <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" on:click={connectWallet} class="{$buttonColors} px-8 h-full md:py-0 py-6 md:w-auto w-full flex items-center justify-center 2xl:flex 2xl:font-mono 2xl:font-light">
+            <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" on:click={connectWallet} class="px-8 h-full md:py-0 py-6 md:w-auto w-full flex items-center justify-center 2xl:flex 2xl:font-mono 2xl:font-light">
                   Connect your wallet
             </button>
           {/if} 
 
             {#if $isConnected}
               <Dropdown triggerElement={dropdownTrigger}>
-                <button type="button" class="{$buttonColors} px-8 h-full md:py-0 py-6 md:w-auto w-full flex items-center justify-center 2xl:flex 2xl:font-mono 2xl:font-light" bind:this={dropdownTrigger}>
+                <button type="button" class="px-8 h-full md:py-0 py-6 md:w-auto w-full flex items-center justify-center 2xl:flex 2xl:font-mono 2xl:font-light" bind:this={dropdownTrigger}>
                     Connected
+                    <span class="after:absolute after:inset-y-0 after:right-0 after:flex after:items-center after:pr-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z" clip-rule="evenodd" />
+                      </svg>
+                    </span>
                 </button>
                 <div slot="DropdownMenu">
-                    <button on:click={disconnectWallet} class="dropdown-item" type="button">Disconnect</button>
+                    <button on:click={disconnectWallet} class="dropdown-item 2xl:font-mono 2xl:font-light" type="button">Disconnect</button>
                 </div>
                 </Dropdown>
             {/if}
--- dotsamastake/src/components/Home.svelte
+++ dotsamastake/src/components/Home.svelte
@@ -10,6 +10,6 @@ let currentMessage = "";
 <div class="2xl:contents lg:contents md:contents flex justify-center items-center h-screen tails-selected-element" contenteditable="true">
   <h1 spellcheck="false" class="w-auto text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tighter inline-block text-left md:text-center relative lg:grid 2xl:font-mono 2xl:font-medium xl:font-mono xl:font-medium lg:font-medium lg:font-mono md:font-medium md:font-mono sm:font-medium sm:font-mono xl:relative md:inline sm:container sm:items-start sm:w-full sm:text-center 2xl:py-60 xl:py-60 lg:py-60 md:py-60 sm:pt-60 mx-auto">
     Reliable validator nodes on 
-        <div class="inline-block animate-typing">Kusama</div>
+        <div class="inline-block animate-typing md:text-center" >Kusama</div>
   </h1>
 </div>
